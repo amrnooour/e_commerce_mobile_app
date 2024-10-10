b9eee5dcd67a72c69ca2941c4aeacad960c60a48
Your Name <you@example.com>
2024-10-10 23:22:07 +0300

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:e_commerce_mobile_app/core/configs/theme/app_theme.dart';
 import 'package:e_commerce_mobile_app/core/router/app_router.dart';
+import 'package:e_commerce_mobile_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
