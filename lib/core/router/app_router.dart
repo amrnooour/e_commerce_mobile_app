2d4c67c10168ecdcd4f3410ea643d0fe66bd3863
Your Name <you@example.com>
2024-10-10 14:24:18 +0300

    finish splash screen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,10 @@
+import 'package:e_commerce_mobile_app/core/constants.dart/routes_path.dart';
+import 'package:e_commerce_mobile_app/presentation/auth/pages/signin.dart';
+import 'package:e_commerce_mobile_app/presentation/splash/pages/splash.dart';
 import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(routes: [
-  GoRoute(path: "/",),
+  GoRoute(path: "/",builder: (context, state) => const Splash(),),
+  GoRoute(path: RoutesPath.signinRoute,builder: (context, state) => const Signin(),),
+
 ]);
--- lib/presentation/splash/pages/splash.dart
+++ lib/presentation/splash/pages/splash.dart
@@ -1,4 +1,9 @@
+import 'package:e_commerce_mobile_app/core/configs/assets/app_vectors.dart';
+import 'package:e_commerce_mobile_app/core/configs/theme/app_colors.dart';
+import 'package:e_commerce_mobile_app/core/constants.dart/routes_path.dart';
+import 'package:e_commerce_mobile_app/core/router/navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Splash extends StatefulWidget {
   const Splash({super.key});
@@ -10,11 +15,19 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   void initState() {
-    // TODO: implement initState
+    Future.delayed(const Duration(seconds: 2), () {
+      customNavigation(context, RoutesPath.signinRoute);
+    });
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppColors.primary,
+      body: Center(
+        child: SvgPicture.asset(AppVectors.logo),
+      ),
+    );
   }
 }
